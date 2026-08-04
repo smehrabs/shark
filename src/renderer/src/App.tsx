8a7a58e8e42805d7fc407d8dcb5eca2e5f3346bd
mrb <greymon647@gmail.com>
2026-08-04 22:50:31 +0330

    cleanup

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,13 @@
+function Home(): React.JSX.Element {
+  return (
+    <div style={{ backgroundColor: '#1a1a1a', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', scrollbarWidth: 'none'  , scrollbarColor: 'transparent transparent' }}>
+      <h1 style={{ color: '#fff' }}>Welcome to the Home Page</h1>
+    </div>
+  )
+}
+
 function App(): React.JSX.Element {
-  return <div style={{ backgroundColor: '#1a1a1a' }} />
+  return <Home />
 }
 
 export default App
